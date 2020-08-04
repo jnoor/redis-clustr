6ca7f5ff57785f4eaee28f120454d9786889077a
Joseph Noor <josephgnoor@gmail.com>
2020-08-03 19:59:59 -0700

    updating log sketch to support quantile sketching

--- src/LogSketch.js
+++ src/LogSketch.js
@@ -5,17 +5,37 @@ var lastStore = undefined;
 var redis = undefined;
 var addedClientToRedis = false;
 
-//NOTE: THIS ONLY SUPPORTS CONNECTIONS TO ONE REDIS INSTANCE AT A TIME...
-//TODO: Should only sketch one window at a time???
+const { DDSketch } = require('sketches-js');
+
+var optimizationMetric = '99'
+// var optimizationMetric = 'avg'
+
+//this is for a single cluster
+//sketch one window at a time?
 
 module.exports.printLog = function(cmd, elapsed) {
   Object.keys(sketch).forEach(function(node) {
-    var sum = sketch[node].sum
     var cnt = sketch[node].cnt
-    console.log("Accessed", node, cmd, cnt, "times. Took", sum / cnt, "ms on average...");
+    var latency = getRequestLatency(node);
+    console.log("Accessed", node, cmd, cnt, "times. Took", latency, "ms...");
   });
   console.log("Accessed ", Object.keys(sketch.slots).length, " slots:", Object.keys(sketch.slots));
 };
+
+var getStorableSketch = function() {
+  var storableSketch = {slots:{}}
+  Object.keys(sketch).forEach(function(node) {
+    if(node == 'time' || node == 'slots') {
+      storableSketch[node] = sketch[node]
+    } else {
+      storableSketch[node] = {
+        cnt: sketch[node].cnt,
+        sum: getRequestLatency(node) * sketch[node].cnt
+      }
+    }
+  });
+  return JSON.stringify(storableSketch);
+}
 
 var storing = false;
 var clientID = require('uuid/v1')();
@@ -27,7 +47,7 @@ var storeSketch = function() {
   sketch['time'] = Date.now()
 
   //store sketch
-  redis.setex(clientID, 600, JSON.stringify(sketch), function(err, resp) {
+  redis.setex(clientID, 600, getStorableSketch(), function(err, resp) {
     if (err) console.log(err);
     else {
       console.log("set sketch into redis.");
@@ -46,27 +66,53 @@ var storeSketch = function() {
   });
 }
 
+var logLatency = function(host, elapsed_time) {
+  if (optimizationMetric === 'avg') {
+    //average, just keep running count
+    if (sketch[host] === undefined) {
+      sketch[host] = {
+        cnt: 0,
+        sum: 0.0,
+      }
+    }
+    sketch[host].cnt += 1
+    sketch[host].sum += elapsed_time
+  } else {
+    //quantile, use sketch
+    if(sketch[host] === undefined) {
+      sketch[host] = {
+        cnt: 0,
+        ddsketch: new DDSketch({alpha: 0.02}) // compute quantiles with precision of 2 percent
+    }
+    sketch[host].cnt += 1
+    sketch[host].ddsketch.add(elapsed_time)
+  }
+}
+
+var getRequestLatency = function(host) {
+  if(optimizationMetric === 'avg') {
+    return sketch[host].sum / sketch[host].cnt;
+  } else {
+    //just 99%ile for now
+    return sketch[host].ddsketch.quantile(0.99);
+  }
+}
+
 module.exports.logRequest = function(cli, cmd, key, elapsed, RedisClustr) {
   redis = RedisClustr
   //only accept sets and gets (for now...)
   if (cmd != "set" && cmd != "get") return;
   
   console.info(cli.address + " " + cmd + ' took %ds %dms', elapsed[0], elapsed[1] / 1000000)
-  if (sketch[cli.address] === undefined) {
-    sketch[cli.address] = {
-      cnt: 0,
-      sum: 0.0,
-    }
-  }
 
+  //log slot request
   var slot = calculateSlot(key);
   sketch.slots[slot] = (sketch.slots[slot] || 0) + 1;
 
-  //hardset to all for now
-  sketch[cli.address].cnt += 1
-  sketch[cli.address].sum += elapsed[0]*1000 + elapsed[1] / 1000000
+  //log network request
+  logLatency(cli.address, elapsed[0]*1000 + elapsed[1] / 1000000);
 
-  //check if i should dump to redis
+  //check if should dump to redis
   if (!lastStore) lastStore = process.hrtime();
   var sketchDumpPeriod = 10 //number of seconds to dump
   if (process.hrtime(lastStore)[0] > sketchDumpPeriod) {
